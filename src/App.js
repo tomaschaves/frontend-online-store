3d1785821320460f061d558e1742c24cdbb16d6b
Tomás Chaves <tomcchaves@gmail.com>
2023-01-11 16:50:57 -0300

    Requisito 3.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import './App.css';
 import ProductsList from './components/ProductsList';
+import ShoppingCart from './components/ShoppingCart';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <ProductsList />
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ ProductsList } />
+          <Route exact path="/cart" component={ ShoppingCart } />
+        </Switch>
+        <Link to="/cart" data-testid="shopping-cart-button">Carrinho</Link>
+      </BrowserRouter>
     );
   }
 }
