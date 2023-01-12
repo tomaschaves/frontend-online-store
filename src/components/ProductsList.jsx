17b604190c52b4b6ba49789b958f22a3d40b4ddb
Tomás Chaves <tomcchaves@gmail.com>
2023-01-12 19:01:40 -0300

    Requisito 8.

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -4,6 +4,35 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 class ProductsList extends Component {
+  getSavedProducts = () => {
+    const cartProducts = localStorage.getItem('products');
+    return cartProducts ? JSON.parse(cartProducts) : [];
+  };
+
+  saveProduct = (title, thumbnail, price, id) => {
+    const cartProducts = this.getSavedProducts();
+    // se o id já existir, vamos aumentar o qtd, se não existir, vamos setar em 1 e aumentar a quantidade de produtos no carrinho
+    const has = cartProducts.some((element) => element.id === id);
+    if (has) {
+      const existingProduct = cartProducts.map((element) => {
+        if (element.id === id) {
+          element.qtd += 1;
+        }
+        return element;
+      });
+      localStorage.setItem('products', JSON.stringify(existingProduct));
+    } else {
+      const newCartProducts = [...cartProducts, {
+        title,
+        thumbnail,
+        price,
+        id,
+        qtd: 1,
+      }];
+      localStorage.setItem('products', JSON.stringify(newCartProducts));
+    }
+  };
+
   render() {
     const { list, click } = this.props; // Click é uma variável para vermos se já buscamos alguma vez. Caso afirmativo, o texto 'Nenhum produto foi encontrado' será renderizado quando não tiver resultados. Caso contrário, ao acabar de entrar na página, mesmo tendo um array de respostas vazio, ele não será renderizado, pois não terá sido feita nenhuma pesquisa.
 
@@ -23,6 +52,13 @@ class ProductsList extends Component {
               >
                 Detalhes
               </Link>
+              <button
+                type="button"
+                onClick={ () => this.saveProduct(title, thumbnail, price, id) }
+                data-testid="product-add-to-cart"
+              >
+                Adicionar ao carrinho
+              </button>
             </div>
           )) }
       </div>
