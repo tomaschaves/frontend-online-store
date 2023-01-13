fdd5634f77e0c2ac7e7e52ee19ca5017e07686c7
Soleane Mota <sem.sol@gmail.com>
2023-01-13 15:10:27 -0400

    Requisito 10

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -6,20 +6,51 @@ class ShoppingCart extends Component {
   };
 
   componentDidMount() {
+    this.getState();
+  }
+
+  getState = () => {
     this.setState({
       cartProducts: this.getSavedProducts(),
     });
-  }
+  };
 
   getSavedProducts = () => {
     const cartProducts = localStorage.getItem('products');
     return cartProducts ? JSON.parse(cartProducts) : [];
   };
 
+  addAndRemoveProducts = (id, operation, qtd) => {
+    const cartProducts = this.getSavedProducts();
+    const existingProduct = cartProducts.map((element) => {
+      if (element.id === id && operation === 'plus') {
+        element.qtd += 1;
+      } else if (element.id === id && operation === 'minus' && qtd > 1) {
+        element.qtd -= 1;
+      }
+      return element;
+    });
+    localStorage.setItem('products', JSON.stringify(existingProduct));
+    this.getState();
+  };
+
+  deleteItem = (id) => {
+    const cartProducts = this.getSavedProducts();
+    const filterElement = cartProducts.filter((element) => element.id !== id);
+    localStorage.setItem('products', JSON.stringify(filterElement));
+    this.getState();
+  };
+
   render() {
     const { cartProducts } = this.state;
     return (
       <div>
+        <button
+          type="button"
+          data-testid="shopping-cart-button"
+        >
+          Carrinho
+        </button>
         {
           (!cartProducts || cartProducts.length === 0) ? (
             <p data-testid="shopping-cart-empty-message">
@@ -31,7 +62,29 @@ class ShoppingCart extends Component {
                 <h1 data-testid="shopping-cart-product-name">{ title }</h1>
                 <h2>{ price }</h2>
                 <img src={ thumbnail } alt={ title } />
+                <button
+                  type="button"
+                  data-testid="product-increase-quantity"
+                  onClick={ () => this.addAndRemoveProducts(id, 'plus', qtd) }
+                >
+                  +
+                </button>
                 <p data-testid="shopping-cart-product-quantity">{ qtd }</p>
+                <button
+                  type="button"
+                  data-testid="product-decrease-quantity"
+                  onClick={ () => this.addAndRemoveProducts(id, 'minus', qtd) }
+                >
+                  -
+                </button>
+                <button
+                  type="button"
+                  data-testid="remove-product"
+                  onClick={ () => this.deleteItem(id) }
+                >
+                  Remover
+
+                </button>
               </div>
             ))
           )
